2da6646aecf3099cda05efd72c01042a55e70926
shadovNik <chebanenko.niki@mail.ru>
2026-04-30 23:52:36 +0500

    test commit

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -1,7 +1,16 @@
 
 function Test() {
     return (
-        <div className="hero"></div>
+        <>
+            <div className="hero"></div>
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hello! I am Nikita</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Hello! I am Nikita</button>
+                </div>
+            </div>
+        </>
     );
 }
 
